a50bcc7ceb6be473b41bb4eca8fa6412bf31b153
Akilanpalani <akilanpalani2001@gmail.com>
2024-03-25 23:19:01 +0530

    initial-commit

---
+++ src/components/providers/modal-providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import {useEffect, useState} from 'react';
+import {CreateModal} from "@/components/modals/create-modal";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, [isMounted]);
+
+  if(!isMounted) {
+    return null
+  }
+
+  return(
+    <CreateModal />
+  )
+
+}
